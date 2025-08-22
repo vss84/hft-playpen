ec3a9fb41d0c342c60a9aed7af2e9dee194e1083
vss <vssorroche@gmail.com>
2025-08-22 19:42:35 -0300

    Add ring buffer tests

--- ring_buffer/tests/test_ring_buffer.cpp
+++ ring_buffer/tests/test_ring_buffer.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
+#include <thread>
 
 #include "ring_buffer.h"
-#include <iostream>
 
 using namespace hft;
 
@@ -19,16 +19,48 @@ TEST(SPSCRingBuffer, BasicTest)
     for (int i = 0; i < 15; ++i)
     {
         int buffer;
-        std::cout << "pre pop: " << rb.Peek(i);
-
         auto result = rb.Pop(buffer);
-        std::cout << " post pop: " << rb.Peek(i) << "\n";
-        std::cout << " result: " << result << "\n";
-
         ASSERT_TRUE(result);
         EXPECT_EQ(buffer, i);
     }
 
     int temp;
     ASSERT_FALSE(rb.Pop(temp));
+}
+
+TEST(SPSCRingBuffer, BasicTest2)
+{
+
+    SPSCRingBuffer<int, 1024> rb;
+    constexpr int items = 10'000'000;
+
+    std::thread producer(
+        [&]
+        {
+
+            for (int i = 0; i < items; ++i)
+            {
+                while (!rb.Push(i))
+                {
+                    std::this_thread::yield();
+                }
+            }
+        });
+
+    std::thread consumer(
+        [&] 
+        { 
+            for (int expected = 0; expected < items; ++expected) 
+            {  
+                int value;
+                while (!rb.Pop(value)) 
+                {
+                    std::this_thread::yield();
+                }
+                EXPECT_EQ(value, expected);
+            }
+        });
+
+    producer.join();
+    consumer.join();
 }
